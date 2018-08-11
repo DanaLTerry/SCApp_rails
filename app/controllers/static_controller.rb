bfae661e1ec7b74a434033f9c0fa9d05500c0fbb
Dana Terry <Dana@Danas-MacBook-Air.local>
2018-08-11 17:40:08 -0400

    static pages up

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,26 @@
+class StaticController < ApplicationController
+
+    def home
+    end
+
+    def medical
+    end
+
+    def dental
+    end
+
+    def education
+    end
+
+    def team
+    end
+
+    def FAQ
+    end
+
+    def COC
+    end
+
+    def payment
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  get 'home' => 'static#home'
+  get 'medical' => 'static#medical'
+  get 'dental' => 'static#dental'
+  get 'education' => 'static#education'
+  get 'team' => 'static#team'
+  get 'FAQ' => 'static#FAQ'
+  get 'COC' => 'static#COC'
+  get 'payment' => 'static#payment'
 end
